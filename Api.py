5958421fde07dab58398cad60e13b1dae072baf9
Guenael.Bournac <guenael.bournac@insa-rennes.fr>
2019-10-31 15:34:49 +0100

    [API] mis au propre du code de l'API

--- Api.py
+++ Api.py
@@ -1,17 +1,36 @@
 import requests
 import json
 import datetime
-
-def requete():
-    r = requests.get("https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-circulation-passages-tr&sort=-arrivee&facet=idligne&facet=nomcourtligne&facet=sens&facet=destination&facet=precision&refine.nomcourtligne=C4&refine.destination=Grand+Quartier&refine.precision=Temps+r%C3%A9el&refine.idarret=1292")
+"""
+Star Api Request for Real Time hours
+"""
+def requete(ligne = "C4", destination = "Grand+Quartier", idArret = "1292"):
+    r = requests.get("https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-circulation-passages-tr&sort=-arrivee&facet=idligne&facet=nomcourtligne&facet=sens&facet=destination&facet=precision&" +
+    "refine.nomcourtligne=" + ligne + "&refine.destination=" + destination + "&" +
+    "refine.precision=Temps+r%C3%A9el&refine.idarret=" + idArret)
     return r
 
-def nextBuses():
-    r = requete()
+"""
+This functiun gives the next hours for the buses
+"""
+def nextHoursBuses(ligne = "C4", destination = "Grand+Quartier", idArret = "1292" , nbResult = 2):
+    r = requete(ligne, destination, idArret)
     jsonR = json.loads(r.text)
     res = []
-    for rows in jsonR["records"][0:2]:
+    for rows in jsonR["records"][0:nbResult]:
         strArrivee = rows["fields"]["arrivee"]
-        arrive = datetime.datetime.strptime(strArrivee, '%Y-%m-%dT%H:%M:%S+00:00') + datetime.timedelta(hours = 1)
-        res.append(arrive.strftime('%H:%M %d/%m/%Y'))
+        res.append(conversionDate(strArrivee))
     return res
+
+def conversionDate(date):
+    dateObject = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S+00:00') 
+    dateObject = dateObject + datetime.timedelta(hours = 1)
+    return dateObject.strftime('%H:%M %d/%m/%Y')
+
+def Affichage(ligne = "C4", destination = "Grand+Quartier", idArret = "1292", nbResult = 2):
+    r = nextHoursBuses(ligne, destination, idArret, nbResult)
+    print("ligne : " + ligne)
+    print("direction : " + destination.replace("+", " "))
+    print("Arret : " + idArret)
+    for l in r:
+        print(" "+l)
