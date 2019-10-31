4a195d1938002e3ea6a58b260f34f17e389813da
Guenael.Bournac <guenael.bournac@insa-rennes.fr>
2019-10-31 14:09:10 +0100

    [API] fonction qui renvois la date et l'heure des 2 prochains bus

--- Api.py
+++ Api.py
@@ -1,6 +1,17 @@
 import requests
-
+import json
+import datetime
 
 def requete():
-    r = requests.get("https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-circulation-passages-tr&facet=idligne&facet=nomcourtligne&facet=sens&facet=destination&facet=precision&refine.nomcourtligne=C4&refine.destination=Grand+Quartier&refine.precision=Temps+r%C3%A9el&refine.idarret=1292")
+    r = requests.get("https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-circulation-passages-tr&sort=-arrivee&facet=idligne&facet=nomcourtligne&facet=sens&facet=destination&facet=precision&refine.nomcourtligne=C4&refine.destination=Grand+Quartier&refine.precision=Temps+r%C3%A9el&refine.idarret=1292")
     return r
+
+def nextBuses():
+    r = requete()
+    jsonR = json.loads(r.text)
+    res = []
+    for rows in jsonR["records"][0:2]:
+        strArrivee = rows["fields"]["arrivee"]
+        arrive = datetime.datetime.strptime(strArrivee, '%Y-%m-%dT%H:%M:%S+00:00') + datetime.timedelta(hours = 1)
+        res.append(arrive.strftime('%H:%M %d/%m/%Y'))
+    return res
